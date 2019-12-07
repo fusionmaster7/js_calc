981e6ba8f3d9aa49c98fb567f4209fb66307fcdb
Hardik Singh <hardiksingh297@gmail.com>
2019-12-07 22:39:13 +0530

    Finished the code for one-time use of the calculator.

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById('data').value = 0;
+function input(n){
+  var num = document.getElementById('data').value;
+  num = num*10 + n;
+  document.getElementById('data').value = num;
+}
+function input_op(c){
+  document.getElementById('data').value = c;
+}
+function confirm(){
+  if(ctr == 0){
+    num1 = document.getElementById('data').value;
+    ctr+=1;
+    document.getElementById('data').value = 0;
+    alert("Please enter the second number");
+  }
+  else if(ctr==1){
+    num2 = document.getElementById('data').value;
+    document.getElementById('data').value= 0;
+    alert("Please enter the operation");
+    ctr+=1;
+  }
+  else{
+    c = document.getElementById('data').value;
+    if(c=='+'){
+      document.getElementById('data').value = Number(num1) + Number(num2);
+    }
+    else if(c=='-'){
+      document.getElementById('data').value = Number(num1) - Number(num2);
+    }
+    else if(c=='*'){
+      document.getElementById('data').value = Number(num1) * Number(num2);
+    }
+    else {
+      document.getElementById('data').value = Number(num1) / Number(num2);
+    }
+  }
+}
+function reset(){
+  document.getElementById('data').value = 0;
+}
+alert("Please enter the first number");
+var ctr = 0;
+var num1 = 0;
+var num2 = 0;
